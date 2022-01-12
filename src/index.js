667ab9b0d9327919b03b9e55021894063d25815b
Marina Belenkaya <marina_vorobyeva1423@mail.ru>
2022-01-12 23:16:05 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+  let mod = Math.abs(n);
+  let str = String(mod);
+  let arr = str.split ('');
+  let reverseArr = arr.reverse();
+  let joinArr = reverseArr.join('');
+  let num = Number(joinArr);
+  return(num);
+
   
 }
